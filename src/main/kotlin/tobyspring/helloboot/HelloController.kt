8c4ca6ff097f7d6cacd2ec53cfcb583bd7d23cc4
kkyu8925 <kkyu8925@gmail.com>
2023-04-15 20:02:20 +0900

    독립 실행형 서블릿 애플리케이션

--- src/main/kotlin/tobyspring/helloboot/HelloController.kt
+++ src/main/kotlin/tobyspring/helloboot/HelloController.kt
@@ -1,12 +1,7 @@
 package tobyspring.helloboot
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
-
-@RestController
 class HelloController {
 
-    @GetMapping("/hello")
     fun hello(name: String): String {
         return "Hello $name"
     }
