79787d68a11bf13185ea392b5f474789a1eaa6eb
kkyu8925 <kkyu8925@gmail.com>
2023-04-25 23:31:40 +0900

    섹션 9. Spring JDBC 자동 구성 개발

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.net.URI
 
 plugins {
     id("org.springframework.boot") version "2.7.10"
@@ -13,11 +14,18 @@ java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
+    maven {
+        url = URI("https://repo.clojars.org")
+        name = "Clojars"
+    }
 }
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
 //    implementation("org.springframework.boot:spring-boot-starter-jetty")
+    implementation("org.springframework:spring-jdbc")
+    runtimeOnly("com.h2database:h2:2.1.214")
+    implementation("hikari-cp:hikari-cp:3.0.1")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
